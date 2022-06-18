7a42adf35b3da2b1c06ad73cf0cbff5f16d51cbf
saul313 <39901144+saul313@users.noreply.github.com>
2022-06-18 16:07:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: '学习园地',
-  author: 'Craig Hart',
+  author: 'saul shi',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
