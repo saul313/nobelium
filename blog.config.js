12ba18c932dc20a5eb15afd7a7090a9faa54af20
saul313 <39901144+saul313@users.noreply.github.com>
2022-06-26 17:20:08 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -55,7 +55,7 @@ const BLOG = {
       scriptSrc: 'https://cusdis.com/js/cusdis.es.js' // change this if you're using self-hosted version
     }
   },
-  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
+  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: )
 }
 // export default BLOG
 module.exports = BLOG
